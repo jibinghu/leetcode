dc687fd5009b95019d41dddcae1b072023d4b196
jibinghu <544575367@qq.com>
2024-11-01 17:05:08 +0800

    24/11/1-2

--- Algorithm/prefix_infix_postfix_ex/infix2postfix_.cpp
+++ Algorithm/prefix_infix_postfix_ex/infix2postfix_.cpp
@@ -25,6 +25,20 @@ int precedence(char op) {
 
 // 将中缀表达式转换为后缀表达式
 std::string infixToPostfix(const std::string& infix) {
+    /*
+    中缀表达式转后缀表达式关键点：
+    - 需要一个栈和一个string表达式 -
+    1. 遇到数字直接输出；
+    2. 遇到左括号直接进栈；
+    3. 遇到右括号将栈内左括号之前的所有操作符弹出；
+    4. 遇到操作符，将栈内所有优先级高的操作符弹出后再进栈。
+    - 栈的方法：
+    1. stack.top();
+    2. stack.pop();
+    3. stack.push();
+    4. stakc.empty();
+    5. stack.
+    */ 
     std::stack<char> operators;
     std::string postfix;
     for (char ch : infix) {
@@ -55,6 +69,11 @@ std::string infixToPostfix(const std::string& infix) {
 
 // 根据后缀表达式构建表达式二叉树
 TreeNode* constructTree(const std::string& postfix, int& index) {
+    /*
+    构建表达式二叉树一般都是用递归的方法(从后往前递归构建二叉树)：
+    如果当前字符是操作符，创建节点 node，然后递归构建 right 和 left 子节点，表示该操作符的操作数；
+	如果当前字符是操作数，直接返回对应的节点
+    */
     if (index < 0) return nullptr;
     char ch = postfix[index--];
     TreeNode* node = new TreeNode(ch);
@@ -68,6 +87,7 @@ TreeNode* constructTree(const std::string& postfix, int& index) {
 // 构建表达式二叉树的接口函数
 TreeNode* buildExpressionTree(const std::string& infix) {
     std::string postfix = infixToPostfix(infix);
+    std::cout << "对应的后缀表达式：" << postfix << std::endl;
     int index = postfix.size() - 1;
     return constructTree(postfix, index);
 }
