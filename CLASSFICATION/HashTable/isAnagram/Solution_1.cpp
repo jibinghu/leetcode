7fb10d7049764d6d40d19d58cd8661c1f737e86e
jibinghu <544575367@qq.com>
2024-12-07 11:40:34 +0800

    12/07/01

---
+++ CLASSFICATION/HashTable/isAnagram/Solution_1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        // 解法确实牛，这个字母映射到数字的技巧可以经常用到
+        // 另外数组在C++中也是unordered_map的特例，搜索和增删改效率是O(1)
+        int record[26] = {0};
+        for(int i=0;i<s.size();i++){
+            record[s[i]-'a']++;
+        }
+        for(int j=0;j<t.size();j++){
+            record[t[j]-'a']--;
+        }
+        for(int k=0;k<26;k++){
+            if(record[k]!=0)
+                return false;
+        }
+        return true;
+    }
+};
